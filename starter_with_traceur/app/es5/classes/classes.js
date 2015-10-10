d1ac126cf1d3040c3898b11ff3471e9cd2f11b5e
sifeij <sifeij@gmail.com>
2015-10-10 12:45:46 -0700

    Classes inheritance

--- starter_with_traceur/app/es5/classes/classes.js
+++ starter_with_traceur/app/es5/classes/classes.js
@@ -1,6 +1,10 @@
 "use strict";
 
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -69,7 +73,7 @@ describe("the class keyword", function () {
 			function Employee(name) {
 				_classCallCheck(this, Employee);
 
-				this._name = name;
+				this.name = name;
 			}
 
 			_createClass(Employee, [{
@@ -100,6 +104,193 @@ describe("the class keyword", function () {
 
 		e1.name = "Chris";
 		expect(e1.name).toBe("CHRIS");
+	});
+
+	it("can have a super class", function () {
+		var Person = (function () {
+			function Person(name) {
+				_classCallCheck(this, Person);
+
+				this.name = name;
+			}
+
+			_createClass(Person, [{
+				key: "name",
+				get: function get() {
+					return this._name;
+				},
+				set: function set(newValue) {
+					if (newValue) {
+						this._name = newValue;
+					}
+				}
+			}]);
+
+			return Person;
+		})();
+
+		var Employee = (function (_Person) {
+			_inherits(Employee, _Person);
+
+			function Employee() {
+				_classCallCheck(this, Employee);
+
+				_get(Object.getPrototypeOf(Employee.prototype), "constructor", this).apply(this, arguments);
+			}
+
+			_createClass(Employee, [{
+				key: "doWork",
+				value: function doWork() {
+					return this._name + " is working";
+				}
+			}]);
+
+			return Employee;
+		})(Person);
+
+		var p1 = new Person("Sophie");
+		var e1 = new Employee("Christopher");
+
+		expect(p1.name).toBe("Sophie");
+		expect(e1.name).toBe("Christopher");
+		expect(e1.doWork()).toBe("Christopher is working");
+	});
+
+	it("can invoke super method", function () {
+		var Person = (function () {
+			function Person(name) {
+				_classCallCheck(this, Person);
+
+				this.name = name;
+			}
+
+			_createClass(Person, [{
+				key: "name",
+				get: function get() {
+					return this._name;
+				},
+				set: function set(newValue) {
+					if (newValue) {
+						this._name = newValue;
+					}
+				}
+			}]);
+
+			return Person;
+		})();
+
+		var Employee = (function (_Person2) {
+			_inherits(Employee, _Person2);
+
+			function Employee(title, name) {
+				_classCallCheck(this, Employee);
+
+				_get(Object.getPrototypeOf(Employee.prototype), "constructor", this).call(this, name);
+				this._title = title;
+			}
+
+			_createClass(Employee, [{
+				key: "doWork",
+				value: function doWork() {
+					return this._name + " is working";
+				}
+			}, {
+				key: "title",
+				get: function get() {
+					return this._title;
+				}
+			}]);
+
+			return Employee;
+		})(Person);
+
+		var e1 = new Employee("Developer", "Christopher");
+		expect(e1.name).toBe("Christopher");
+		expect(e1.title).toBe("Developer");
+	});
+
+	it("can override methods", function () {
+		var Person = (function () {
+			function Person(name) {
+				_classCallCheck(this, Person);
+
+				this.name = name;
+			}
+
+			_createClass(Person, [{
+				key: "doWork",
+				value: function doWork() {
+					return "free";
+				}
+			}, {
+				key: "toString",
+				value: function toString() {
+					return this.name;
+				}
+			}, {
+				key: "name",
+				get: function get() {
+					return this._name;
+				},
+				set: function set(newValue) {
+					if (newValue) {
+						this._name = newValue;
+					}
+				}
+			}]);
+
+			return Person;
+		})();
+
+		var Employee = (function (_Person3) {
+			_inherits(Employee, _Person3);
+
+			function Employee(title, name) {
+				_classCallCheck(this, Employee);
+
+				_get(Object.getPrototypeOf(Employee.prototype), "constructor", this).call(this, name);
+				this._title = title;
+			}
+
+			_createClass(Employee, [{
+				key: "doWork",
+				value: function doWork() {
+					return _get(Object.getPrototypeOf(Employee.prototype), "doWork", this).call(this) + "paid";
+				}
+			}, {
+				key: "title",
+				get: function get() {
+					return this._title;
+				}
+			}]);
+
+			return Employee;
+		})(Person);
+
+		var e1 = new Employee("Developer", "Sophie");
+		var p1 = new Person("Alex");
+
+		expect(p1.doWork()).toBe("free");
+		expect(e1.doWork()).toBe("freepaid");
+		expect(p1.toString()).toBe("Alex");
+		expect(e1.toString()).toBe("Sophie");
+
+		var makeEveryoneWork = function makeEveryoneWork() {
+			var results = [];
+
+			for (var _len = arguments.length, people = Array(_len), _key = 0; _key < _len; _key++) {
+				people[_key] = arguments[_key];
+			}
+
+			for (var i = 0; i < people.length; i++) {
+				if (people[i] instanceof Person) {
+					results.push(people[i].doWork());
+				}
+			};
+			return results;
+		};
+
+		expect(makeEveryoneWork(p1, e1, {})).toEqual(["free", "freepaid"]);
 	});
 });
 //# sourceMappingURL=classes.js.map
--- starter_with_traceur/app/es6/classes/classes.js
+++ starter_with_traceur/app/es6/classes/classes.js
@@ -78,4 +78,143 @@ describe("the class keyword", function(){
 		e1.name = "Chris";
 		expect(e1.name).toBe("CHRIS");
 	});
+	
+	it("can have a super class", function(){
+
+		class Person {
+			
+			constructor(name){
+				this.name = name;	
+			}
+			
+			get name() {
+				return this._name;
+			}
+			
+			set name(newValue) {
+				if(newValue){
+					this._name = newValue;
+				}
+			}
+		}
+		
+		class Employee extends Person {
+			
+			doWork() {
+				return `${this._name} is working`;
+			}
+		}
+		
+		let p1 = new Person("Sophie");
+		let e1 = new Employee("Christopher");
+		
+		expect(p1.name).toBe("Sophie");
+		expect(e1.name).toBe("Christopher");
+		expect(e1.doWork()).toBe("Christopher is working");
+	});
+	
+	it("can invoke super method", function(){
+
+		class Person {
+			
+			constructor(name){
+				this.name = name;	
+			}
+			
+			get name() {
+				return this._name;
+			}
+			
+			set name(newValue) {
+				if(newValue){
+					this._name = newValue;
+				}
+			}
+		}
+		
+		class Employee extends Person {
+			
+			constructor(title, name){
+				super(name);
+				this._title = title;
+			}
+			
+			get title(){
+				return this._title;
+			}
+			
+			doWork() {
+				return `${this._name} is working`;
+			}
+		}
+		
+		let e1 = new Employee("Developer", "Christopher");
+		expect(e1.name).toBe("Christopher");
+		expect(e1.title).toBe("Developer");
+		
+	});
+	
+		it("can override methods", function(){
+
+		class Person {
+			
+			constructor(name){
+				this.name = name;	
+			}
+			
+			get name() {
+				return this._name;
+			}
+			
+			set name(newValue) {
+				if(newValue){
+					this._name = newValue;
+				}
+			}
+			
+			doWork() {
+				return "free";
+			}
+			
+			toString() {
+				return this.name;
+			}
+		}
+		
+		class Employee extends Person {
+			
+			constructor(title, name){
+				super(name);
+				this._title = title;
+			}
+			
+			get title(){
+				return this._title;
+			}
+			
+			doWork() {
+				return super.doWork() + "paid";
+			}
+		}
+		
+		let e1 = new Employee("Developer", "Sophie");
+		let p1 = new Person("Alex");
+		
+		expect(p1.doWork()).toBe("free");
+		expect(e1.doWork()).toBe("freepaid");
+		expect(p1.toString()).toBe("Alex");
+		expect(e1.toString()).toBe("Sophie");
+		
+		let makeEveryoneWork = function(...people){
+			var results = [];
+			for(var i=0; i<people.length; i++) {
+				if(people[i] instanceof Person){
+					results.push(people[i].doWork());
+				}
+			};
+			return results;
+		}
+		
+		expect(makeEveryoneWork(p1, e1, {})).toEqual(["free", "freepaid"]);
+	});
 });
